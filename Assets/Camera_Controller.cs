fe330a5d31fddb9a28a734467b10b9d3e7ce2d2d
PabloM <pabmar31@ucm.es>
2022-02-25 12:29:55 +0100

    Camera_Controller (Falta balancear)

---
+++ Assets/Camera_Controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    [SerializeField]
+    float followSpeed = 1, yPos;
+    
+    [SerializeField]
+    Transform _targetTransform;
+
+    Transform _myTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPos = _targetTransform.position;
+        Vector2 newPos = Vector2.Lerp(_myTransform.position, targetPos, followSpeed * Time.deltaTime);
+
+        Debug.Log(targetPos);
+
+        _myTransform.position = new Vector3(newPos.x, newPos.y + yPos, -15);
+    }
+}
