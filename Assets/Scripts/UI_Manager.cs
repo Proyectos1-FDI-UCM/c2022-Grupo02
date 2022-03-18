834ec1e51192840ec333a37c307958059810d736
neutronbomb23 <92365715+neutronbomb23@users.noreply.github.com>
2022-03-18 15:47:24 +0100

    Revert "Merge branch 'main' of https://github.com/Proyectos1-FDI-UCM/c2022-Grupo02"
    This reverts commit 07a17472fbc19972f873687b8d7c0f5aa6c29d0c, reversing
    changes made to 6bc9bfabfa55bffdfa2e36b333fd4c98a1af7171.

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
-    #region references
-    [SerializeField]
-    private GameObject _pauseMenu;
-    #endregion
+
     #region parameters
     [SerializeField]
     private GameObject[] hearts;
@@ -28,23 +25,9 @@ public class UI_Manager : MonoBehaviour
         }
         //Debug.Log("Buenas tardes");
     }
-
-    public void SetPauseMenu(bool enabled)
-    {
-        _mainMenu.SetActive(enabled); 
-    }
-    
-    public void SetContinueButton(bool enabled)
-    {
-        _continueButton.SetActive(enabled); 
-    }
-
-    /// Calls Game Manager method to Quit Game.
-    public void QuitGame()
-    {
-        GameManager.Instance.QuitGame(); //Instancia el Game Manager (QuitGame).
-    }
     #endregion
+
+
 
 
     void Start()
