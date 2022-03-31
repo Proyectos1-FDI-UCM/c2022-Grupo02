01ac514384d4a7f28c795beb94fd28b36b008b27
neutronbomb23 <dherrezu@ucm.es>
2022-03-31 11:06:36 +0200

    Carpeta de scripts creada. Gravedad ordenador laboratorios "-1"

---
+++ Assets/Scripts/Scottie/InputManager.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    #region references
+    private Character_MovementController _myCharacterMovementController;
+    private CharacterController _myCharacterController;
+    private Transform _myTransform;
+    private JumpController _jumpController;
+    private AttackController _AttackController;
+    #endregion
+
+    #region parameters
+    public int dir;
+    private float change = 6;
+    private int magia = 0;
+    private int vida = 1;
+    public int up = 0;
+    private int salto = 0;
+    #endregion
+
+
+    void Start()
+    {
+        _myCharacterMovementController = GetComponent<Character_MovementController>();
+        _myCharacterController = GetComponent<CharacterController>();
+        _myTransform = GetComponent<Transform>();
+        _jumpController = GetComponent<JumpController>();
+        _AttackController = GetComponent<AttackController>();
+        dir = +1;
+    }
+
+    void Update()
+    {
+        change += Time.deltaTime;
+        Vector3 movementDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+            gameObject.GetComponent<Animator>().SetBool("Walk", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            movementDirection.x = -1.0f;
+            dir = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+           
+            gameObject.GetComponent<Animator>().SetBool("Walk", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            movementDirection.x = 1.0f;
+            dir = +1;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            up = 1;
+            gameObject.GetComponent<Animator>().SetBool("MeleUp", true);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            up = 0;
+            gameObject.GetComponent<Animator>().SetBool("Melé", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(salto);
+            _jumpController.Jump(salto);
+        }
+
+        if (Input.GetMouseButton(1) && magia == 1)
+        {
+            gameObject.GetComponent<Animator>().SetBool("Adistancia", true);
+            _AttackController.Shoot(dir);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameManager.Instance.SetPause();
+        }
+
+        if (change >= 0.3f)// cambiar los booleanos a false tras un tiempo
+        {
+            gameObject.GetComponent<Animator>().SetBool("Adistancia", false);
+            gameObject.GetComponent<Animator>().SetBool("Melé", false);
+            gameObject.GetComponent<Animator>().SetBool("MeleUp", false);
+            change = 0; 
+        } 
+        _myCharacterMovementController.SetDirection(movementDirection);
+    }
+    public void DesSalto()
+    {
+        salto = 1;
+    }
+    public void DesMagia()
+    {
+        magia = 1;
+    }
+}
---
+++ Assets/Scripts/Scottie/JumpController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    #region parameters
+    [SerializeField]
+    float _gravity = 9.8f;
+    [SerializeField]
+    float _jumpSpeed = 0f;
+    Vector3 velocity;
+    private bool uno = true;
+    #endregion
+
+    #region properties
+    float _gravitySpeed;
+    #endregion
+
+    #region referneces
+    CharacterController _myCharacterController;
+    #endregion
+
+    #region methods
+    public void Jump(int salto)
+    {
+        if (_myCharacterController.isGrounded)
+        {
+            gameObject.GetComponent<Animator>().SetBool("StartJumping", true);
+            gameObject.GetComponent<Animator>().SetBool("Walk", false);
+            velocity.y = _jumpSpeed;
+        }
+        else if (!_myCharacterController.isGrounded && salto == 1 && uno)
+        {
+            _gravitySpeed = 0;
+            gameObject.GetComponent<Animator>().SetBool("StartJumping", true);
+            gameObject.GetComponent<Animator>().SetBool("Walk", false);
+            velocity.y = _jumpSpeed;
+            uno = false;
+        }
+
+    }
+
+    public Vector3 SetGravity()
+    {
+        if (!_myCharacterController.isGrounded)
+        {
+            _gravitySpeed -= _gravity * (Time.deltaTime/2.0f);
+            velocity.y -= _gravitySpeed;
+            //Debug.Log(velocity.y);
+        }
+        else
+        {
+            _gravitySpeed = 0.05f;
+            uno = true;
+            //Debug.Log("estoy en el suelo");
+        }
+        return velocity;
+    }
+
+    public void TouchCeiling()
+    {
+        velocity.y = -2;
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myCharacterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (_myCharacterController.isGrounded)
+        {
+            gameObject.GetComponent<Animator>().SetBool("StartJumping", false);
+        }
+    }
+} 
